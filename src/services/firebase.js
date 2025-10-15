8bf75dcfc4d2a57dfbd405089914ff41f6884a6f
ever360 <161760945+ever360@users.noreply.github.com>
2025-10-15 13:09:34 -0500

    Initialize Firebase with configuration and services

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,1 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbmehjJWvvVyCR4z9Okswki9vzujOpL3I",
+  authDomain: "kapital-ptv.firebaseapp.com",
+  projectId: "kapital-ptv",
+  storageBucket: "kapital-ptv.firebasestorage.app",
+  messagingSenderId: "189823184230",
+  appId: "1:189823184230:web:c2d0642a20d3ad8d6eeca7",
+  measurementId: "G-H22HK94MXZ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
 
